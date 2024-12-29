48af74140befc67a28a9e4de16f1faa80afda7aa
MunaTurki <103526172+MunaTurki@users.noreply.github.com>
2024-12-29 20:27:58 +0100

    Update EconmyFragmentation.jsx

--- src/sections/EconmyFragmentation.jsx
+++ src/sections/EconmyFragmentation.jsx
@@ -19,7 +19,7 @@ const EconmyFragmentation = () => {
       <p>
       The Houthis, who controled 70 – 80 % of the economy by population density, <a href="https://www.reuters.com/article/world/yemens-houthis-criticise-exiled-governments-central-bank-move-idUSKCN11Q24Q/" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>rejected</a> the relocation and refused to hand over taxes revenues to the IRG in Aden while demanding payroll for public employees in territories under their control.
       The payroll of civil servants and the structure of the public sector in the Houthi controlled areas remain a pending topic in all negotiations. 
-      Teachers as civil servants in the north <a href="https://documents1.worldbank.org/curated/en/099445406102232164/pdf/IDU02c5915390fb9604bd10ae55053ca415f59f9.pdf?utm_source=chatgpt.com" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>won’t be paid regularly,</a> that’s two-thirds of all public educators many of whom will <a href="https://www.unicef.org/yemen/education" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>drop off</a> pursuing other income generating activities.
+      Teachers as civil servants in the north <a href="https://documents1.worldbank.org/curated/en/099445406102232164/pdf/IDU02c5915390fb9604bd10ae55053ca415f59f9.pdf?utm_source=chatgpt.com" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>won’t be paid regularly,</a> that’s two-thirds of all public educators many of whom will drop off pursuing other income generating activities.
       </p>
       </article>
       </div>
