7189fc6831f97679f0e9c6deec62931764c30faa
MunaTurki <103526172+MunaTurki@users.noreply.github.com>
2024-12-29 13:21:10 +0100

    Update YemenisFaring.jsx

--- src/sections/YemenisFaring.jsx
+++ src/sections/YemenisFaring.jsx
@@ -28,10 +28,11 @@ const YemenisFaring = () => {
        “The combined hunger-humanitarian crisis does not result from a lack of basic goods […]. 
       Rising prices have been the primary problem since 2018”. This situation has been largely driven by actions taken by the Houthis and the IRG to undermine rivals, but to undermine the very ability of Yemenis to survive. 
       </p>
-      </p>
-      </p>
+      This is a draft report powered by data written for the application of data journalism position at DW by Muna Turki. To access the raw files, please follow the Link.
 
-       This is a draft report powered by data written for the application of data journalism position at DW by Muna Turki. To access the raw files, please follow the Link.
+      </p>
+      
+
       </article>
       </div>
 
