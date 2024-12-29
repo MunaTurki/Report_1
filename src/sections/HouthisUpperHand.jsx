c6de2d2db3b84447a8a152ebb315d8855dc00f42
MunaTurki <103526172+MunaTurki@users.noreply.github.com>
2024-12-29 18:47:47 +0100

    Update HouthisUpperHand.jsx

--- src/sections/HouthisUpperHand.jsx
+++ src/sections/HouthisUpperHand.jsx
@@ -18,13 +18,13 @@ const HouthisUpperHand = () => {
       </p>
       <p>
       Without oil or sufficient import taxes, the IRG is struggling to maintain sufficient revenues for the budget needed to run its territories. The anti-Houthi front is also fragmented, with multiple actors claiming sovereignty over different parts of the south. Poor performance and <a href="https://www.state.gov/reports/2024-fiscal-transparency-report/yemen/" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>failure to uphold anti-corruption measures</a> led to <a href="https://south24.net/news/news.php?nid=3109" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>delays</a> in promised grants,
-      and the IRG will often find itself unable to issue payroll or keep basic services running. <a href="https://www.alayyam.info/news/9ZY6DWQO-N85H3G-BA47" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>Protests are taking over major cities in the south</a> , 
+      and the IRG will often find itself unable to issue payroll or keep basic services running. <a href="https://www.alayyam.info/news/9ZY6DWQO-N85H3G-BA47" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>Protests are taking over major cities in the south</a>, 
       with a widespread sense of uncertainty about the future.
   
       </p>
       <p>
       The Houthis, who are also unable to provide stable services or payroll while cracking down on critics are increasingly claiming a regional role
-      by attacking maritime traffic through the Red Sea and attacking Israeli cities in support of the Palestinians in the Gaza war. 
+      by attacking maritime traffic through the Red Sea and attacking Israeli cities in support of the Palestinians in Gaza war. 
       The Houthis seem to try to <a href="https://www.dw.com/ar/%D8%A7%D9%84%D9%8A%D9%85%D9%86-%D9%87%D9%83%D8%B0%D8%A7-%D9%8A%D8%B3%D8%AA%D8%BA%D9%84-%D8%A7%D9%84%D8%AD%D9%88%D8%AB%D9%8A%D9%88%D9%86-%D9%87%D8%AC%D9%85%D8%A7%D8%AA-%D8%A7%D9%84%D8%A8%D8%AD%D8%B1-%D8%A7%D9%84%D8%A3%D8%AD%D9%85%D8%B1-%D9%84%D8%AA%D8%B9%D8%B2%D9%8A%D8%B2-%D9%86%D9%81%D9%88%D8%B0%D9%87%D9%85/a-68976670" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>gain popularity</a> in the face of public dissatisfaction with their regime. 
       These latest attacks will add more players to the Yemeni front with the US, Israel and allies targeting the Houthis.
       </p>
