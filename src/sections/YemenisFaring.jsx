5312367ff579eb0414177428cad5317b13f33f97
MunaTurki <103526172+MunaTurki@users.noreply.github.com>
2025-01-02 19:29:28 +0100

    Update YemenisFaring.jsx

--- src/sections/YemenisFaring.jsx
+++ src/sections/YemenisFaring.jsx
@@ -20,9 +20,9 @@ const YemenisFaring = () => {
       The yemenis seem to be on a track of losing more of their means to secure a livelihood, 
         dragged into more conflicts and far from finding an end to long-pending ones.
       The purchase power in the IRG territories is steadily falling, 
-      while the Houthis obstruct international humanitarian efforts by arresting more UN personnel.
-      To track how the livelihood of Yemenis has changed through the last decade, 
-        we use a unit of a basic food basket. A household of 4 will need 10 baskets per month to satisfy minimum caloric intake. 
+      while the Houthis are <a href="https://www.crisisgroup.org/middle-east-north-africa/gulf-and-arabian-peninsula/yemen/brokering-ceasefire-yemens-economic" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>accused of obstructing international humanitarian efforts </a>by arresting more UN and deplomatic personnel.
+      To understand how more that 18 million Yemenis are in need of aid today, the purchase power of a unit of a basic food basket was tracked for the last decade. 
+        A household of 4 will need 10 baskets per month to satisfy minimum caloric intake. 
         An average teacher was able to afford 10 baskets with one-third of their income in 2014 (estimated at 60,000 Riyals). 
         Today, they can only afford one under IRG control and four under Houthi control, 
         assuming that they received their wages regularly to start with.
@@ -30,7 +30,7 @@ const YemenisFaring = () => {
        <LineChart3 />
       <p>
        “The combined hunger-humanitarian crisis does not result from a lack of basic goods […]. 
-      Rising prices have been the primary problem since 2018”, <a href="https://www.crisisgroup.org/middle-east-north-africa/gulf-and-arabian-peninsula/yemen/brokering-ceasefire-yemens-economic" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'> qouting the International Crisis Group</a>. This situation has been largely driven by actions taken by the Houthis and the IRG to undermine each other, but to undermine the very ability of Yemenis to survive. 
+      Rising prices have been the primary problem since 2018”, <a href="https://www.crisisgroup.org/middle-east-north-africa/gulf-and-arabian-peninsula/yemen/brokering-ceasefire-yemens-economic" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'> qouting the International Crisis Group</a>. This situation has been largely driven by actions taken by the Houthis and the IRG to undermine each other economically, but to undermine the very ability of Yemenis to survive. 
       </p>
       <p>
       <i>This draft report has been created by Muna Turki as a sample to apply for DW data journalism position. Please visit the following Github repository for all relevant files for creating this material:  <a href="https://github.com/MunaTurki/Report_1" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>https://github.com/MunaTurki/Report_1</a></i>
