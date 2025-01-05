7f1c0b0accfd8a627184f308b2eeeba426f911c0
MunaTurki <103526172+MunaTurki@users.noreply.github.com>
2025-01-05 17:33:31 +0100

    Update EconmyFragmentation.jsx

--- src/sections/EconmyFragmentation.jsx
+++ src/sections/EconmyFragmentation.jsx
@@ -14,7 +14,7 @@ const EconmyFragmentation = () => {
       className=" relative font-montserrat text-slate-gray leading-8 space-y-1 ">
       <p>
       Pressure was mounting on the international community and the coalition to ensure the flow of basic goods and the survival of the most fragile Yemenis in a country that always struggled with food insecurity.
-      As Kwait peace talks stagnated in 2016, the internationally recognaized governement (IRG) sought to shift economic control away from the Houthis and pave the way for foreign grants by announcing the <a href="https://www.reuters.com/article/business/yemen-president-names-new-central-bank-governor-moves-hq-to-aden-idUSKCN11O0WA/" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>relocation </a> of the Central Bank to Aden. 
+      As Kwait peace talks stagnated in 2016, the internationally recognized governement (IRG) sought to shift economic control away from the Houthis and pave the way for foreign grants by announcing the <a href="https://www.reuters.com/article/business/yemen-president-names-new-central-bank-governor-moves-hq-to-aden-idUSKCN11O0WA/" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>relocation </a> of the Central Bank to Aden. 
       </p>
       <p>
       The Houthis, who controled 70 – 80 % of the economy by population density, <a href="https://www.reuters.com/article/world/yemens-houthis-criticise-exiled-governments-central-bank-move-idUSKCN11Q24Q/" target="_blank" rel="noopener noreferrer" className='text-gray-800 underline'>rejected</a> the relocation and refused to hand over taxes revenues to the IRG in Aden while demanding payroll for public employees in territories under their control.
